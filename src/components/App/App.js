7f61bc95223151507de9aa8625d4e8dadc5ec0b0
Mariusz Jabłoński <amidek321@gmail.com>
2019-10-26 13:28:31 +0200

    poprawa filtrowania kart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import { AnimatedSwitch } from 'react-router-transition';
 import FAQ from '../FAQ/FAQ';
+import styles from './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/info" component={Info} />
         <Route exact path='/FAQ' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
 
 export default App;
+
+<Route exact path='/FAQ' component={FAQ} />;
